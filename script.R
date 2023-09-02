1488d8921b37bf4696436f8202f69f553675ba10
lnfran85 <uc2015223482@student.uc.pt>
2023-09-02 16:07:48 +0100

    updated

--- script.R
+++ script.R
@@ -3,63 +3,58 @@ library(sf)
 library(ggplot2)
 library(ggspatial)
 library(tidyverse)
+library(xlsx)
 
 #Defining paths
 out.dir <- (".\\Monit_Trichi_2023\\Mapas\\")
 
-#loading grid UTM 10x10km ETRS89 / Portugal TM06
+#LOADING DATASETS ----
+
+#Loading grid UTM 10x10km ETRS89 / Portugal TM06
 grid_pt <- st_read(".\\Monit_Trichi_2023\\Monit_Trichilogaster_2023\\Fishnet_10km.shp")
 
 #Loading monitored points
-variables <- c(User = "X1_Usurio", 
-               Data = "X3_Data", 
-               Habitat = "X4_Tipo_de_habitat_co", 
-               Species = "X5_Espcie_que_est_a_r", 
-               Density = "X6_Selecione_a_densid", 
-               Size = "X7_Qual_o_tamanho_dom", 
-               Gall_presence = "X8_A_planta_que_est_a", 
-               Gall_quantity = "X9_Quantas_galhas_obs", 
-               Gall_type = "X10_As_galhas_que_obs")
-
-db <- read.csv("C:\\Users\\LENOVO\\Downloads\\registo-de-trichilogaster-acaciaelongifoliae-csv(14)\\form-1__registo-galhas-de-trichilogaster.csv", header=T, sep=",") %>% 
+db <- read.xlsx(".\\GallsTrichi_Volunteer_20230824.xlsx", "GallsTrichi_Volunteer", header = T, encoding ="UTF-8") %>% 
   as.data.frame() %>%
-  
-  rename(all_of(variables)) %>%
-  
-  mutate_at(c("X12_Observando_a_copa", "X13_Observando_a_copa", "X14_Observando_a_copa", "X15_Observando_a_copa",
-              "User", "Habitat", "Species", "Density", "Size", "Gall_presence", "Gall_type"), as.factor) %>%
-  
-  mutate(X = as.numeric(long_2_Localizao), 
-         Y = as.numeric(lat_2_Localizao),
-         Data = as.Date(Data, format="%d/%m/%Y"),
-         Gall_quantity = as.numeric(Gall_quantity),
-         
-         Density = fct_relevel(Density, "Uma", "Poucas", "Mancha até 100m2", "Mancha de 100m2 a 1ha", "Mancha maior do que 1ha"),
+  select(c(1:36)) %>%
+  #Filtering only Epicollect 5 records during the Monitoring campaign
+  filter(Atividade == "Monitorizacao",
+         Source == "Epicollect 5",
+         between(Date, as.Date('2023-07-01'), as.Date('2023-08-24'))) %>% 
+  #Transforming variables to factor, and latitude and longitude to numeric
+  mutate_at(c(5,13:17,19,20:23), as.factor) %>%
+  mutate_at(c("Y", "X"), as.numeric) %>%
+         #Sorting factor levels
+  mutate(Density = fct_relevel(Density, "Uma", "Poucas", "Mancha até 100m2", "Mancha de 100m2 a 1ha", "Mancha maior do que 1ha"),
          Size = fct_relevel(Size, "Plântula (menor que 1 palmo)", "Planta jovem (até +- 1m)", "Planta adulta"),
-         
-         X12_Observando_a_copa = fct_recode(X12_Observando_a_copa, "0%"="0%","1%-10%"="1-10%","10%-25%"="10-25%","25%-50%"="25%-50%","50%-75%"="50%-75%","75%-90%"="75%-90%","﹥90%"="﹥90% " ), 
-         X13_Observando_a_copa = fct_recode(X13_Observando_a_copa, "0%"="0%","1%-10%"="1%-10%","10%-25%"="10%-25%","25%-50%"="25%-50%","50%-75%"="50%-75%","75%-90%"="75%-90%","﹥90%"="﹥90% " ),
-         X14_Observando_a_copa = fct_recode(X14_Observando_a_copa, "0%"="0%","1%-10%"="1%-10%","10%-25%"="10%-25%","25%-50%"="25%-50%","50%-75%"="50%-75%","75%-90%"="75%-90%","﹥90%"="﹥90% " ), 
-         X15_Observando_a_copa = fct_recode(X15_Observando_a_copa, "0%"="0%","1%-10%"="1%-10%","10%-25%"="10%-25%","25%-50%"="25%-50%","50%-75%"="50%-75%","75%-90%"="75%-90%","﹥90%"="﹥ 90% " ) , 
-         
-         X12_Observando_a_copa = fct_relevel(X12_Observando_a_copa, "0%","1%-10%","10%-25%","25%-50%","50%-75%","75%-90%","﹥90%"), 
-         X13_Observando_a_copa = fct_relevel(X13_Observando_a_copa,  "0%","1%-10%","10%-25%","25%-50%","50%-75%","75%-90%","﹥90%"),
-         X14_Observando_a_copa = fct_relevel(X14_Observando_a_copa,  "0%","1%-10%","10%-25%","25%-50%","50%-75%","75%-90%","﹥90%"), 
-         X15_Observando_a_copa = fct_relevel(X15_Observando_a_copa,  "0%","1%-10%","10%-25%","25%-50%","50%-75%","75%-90%","﹥90%") , 
-         
-         Perc_galls = fct_recode(X12_Observando_a_copa, "0"="0%","5.5"="1%-10%","17.5"="10%-25%","37.5"="25%-50%","62.5"="50%-75%","82.5"="75%-90%","105"="﹥90%" ),
-         Perc_phylo = fct_recode(X13_Observando_a_copa, "0"="0%","5.5"="1%-10%","17.5"="10%-25%","37.5"="25%-50%","62.5"="50%-75%","82.5"="75%-90%","105"="﹥90%" ),
-         Perc_flow = fct_recode(X14_Observando_a_copa, "0"="0%","5.5"="1%-10%","17.5"="10%-25%","37.5"="25%-50%","62.5"="50%-75%","82.5"="75%-90%","105"="﹥90%" ),
-         Perc_pods = fct_recode(X15_Observando_a_copa, "0"="0%","5.5"="1%-10%","17.5"="10%-25%","37.5"="25%-50%","62.5"="50%-75%","82.5"="75%-90%","105"="﹥90%" )) %>% 
-         
+         #Cleaning and sorting factor levels
+         X.Cob_Galls = fct_recode(X.Cob_Galls, "0%"="0%","1%-10%"="1-10%","10%-25%"="10-25%","25%-50%"="25%-50%","50%-75%"="50%-75%","75%-90%"="75%-90%","﹥90%"="﹥90% " ),
+         X.Cob_Galls = fct_relevel(X.Cob_Galls, "0%","1%-10%","10%-25%","25%-50%","50%-75%","75%-90%","﹥90%"), 
+         X.Cob_Phyllodes = fct_recode(X.Cob_Phyllodes, "0%"="0%","1%-10%"="1%-10%","10%-25%"="10%-25%","25%-50%"="25%-50%","50%-75%"="50%-75%","75%-90%"="75%-90%","﹥90%"="﹥90% " ),
+         X.Cob_Phyllodes = fct_relevel(X.Cob_Phyllodes,  "0%","1%-10%","10%-25%","25%-50%","50%-75%","75%-90%","﹥90%"),
+         X.Cob_Flowers = fct_recode(X.Cob_Flowers, "0%"="0%","1%-10%"="1%-10%","10%-25%"="10%-25%","25%-50%"="25%-50%","50%-75%"="50%-75%","75%-90%"="75%-90%","﹥90%"="﹥90% " ),
+         X.Cob_Flowers = fct_relevel(X.Cob_Flowers,  "0%","1%-10%","10%-25%","25%-50%","50%-75%","75%-90%","﹥90%"),
+         X.Cob_Pods = fct_recode(X.Cob_Pods, "0%"="0%","1%-10%"="1%-10%","10%-25%"="10%-25%","25%-50%"="25%-50%","50%-75%"="50%-75%","75%-90%"="75%-90%","﹥90%"="﹥ 90% " ),
+         X.Cob_Pods = fct_relevel(X.Cob_Pods,  "0%","1%-10%","10%-25%","25%-50%","50%-75%","75%-90%","﹥90%"),
+         #Creating new quantitative variables from each level range
+         Perc_galls = fct_recode(X.Cob_Galls, "0"="0%","5.5"="1%-10%","17.5"="10%-25%","37.5"="25%-50%","62.5"="50%-75%","82.5"="75%-90%","105"="﹥90%" ),
+         Perc_phylo = fct_recode(X.Cob_Phyllodes, "0"="0%","5.5"="1%-10%","17.5"="10%-25%","37.5"="25%-50%","62.5"="50%-75%","82.5"="75%-90%","105"="﹥90%" ),
+         Perc_flow = fct_recode(X.Cob_Flowers, "0"="0%","5.5"="1%-10%","17.5"="10%-25%","37.5"="25%-50%","62.5"="50%-75%","82.5"="75%-90%","105"="﹥90%" ),
+         Perc_pods = fct_recode(X.Cob_Pods, "0"="0%","5.5"="1%-10%","17.5"="10%-25%","37.5"="25%-50%","62.5"="50%-75%","82.5"="75%-90%","105"="﹥90%" )) %>% 
+  #Transforming qualitative new variables to numeric       
   mutate_at(c("Perc_galls", "Perc_phylo", "Perc_flow", "Perc_pods"), as.numeric) %>% 
-
+  #Transforming data frame to spatial data frame and projecting from WGS84 to ETRS89 / Portugal TM06
   filter(!is.na(X)) %>%
-  st_as_sf(coords = c("long_2_Localizao","lat_2_Localizao"), crs = 4326, remove = FALSE)
-db <- st_transform(db, crs = 3763 ) #Projecting to ETRS89
+  st_as_sf(coords = c("X","Y"), crs = 4326, remove = FALSE) %>%
+  st_set_crs("+proj=longlat +datum=WGS84") %>%
+  st_transform(crs = 3763) %>%
+  #Joining cell grid codes from UTM to database with monitored points
+  st_join(grid_pt)
 
-#Joining cell grid codes from UTM to database with monitored points
-db <- st_join(db, grid_pt)
+save(db,grid_pt, file = "dataset_monitoring_2023.rds")
+
+
+#CREATING MAPS ----
 
 #PRESENCE OF TRICHILOGASTER
 with_galls <- db %>% 
